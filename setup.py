70a43af679a31710fe0331b074a51ab40cfbd852
ebonnal <bonnal.enzo.dev@gmail.com>
2025-03-13 00:56:56 +0000

    exclude tests from install

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from version import __version__
 setup(
     name="pythoned",
     version=__version__,
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     url="http://github.com/ebonnal/pythoned",
     license="Apache 2.",
     author="ebonnal",
